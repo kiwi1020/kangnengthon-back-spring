f11af519eddf9eee39b9ac28b994155b1e27b01c
kiwi1020 <108050749+kiwi1020@users.noreply.github.com>
2025-01-03 23:57:07 +0900

    feat: Initialize Spring Boot backend project
    Created an initial backend project using Spring Boot.
    
    Added key configuration files and directory structure.

---
+++ src/main/java/com/hackathon/kangnengthon/KangnengthonApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.kangnengthon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KangnengthonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KangnengthonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/kangnengthon/KangnengthonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.kangnengthon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KangnengthonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
